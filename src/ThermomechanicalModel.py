4f15b6cb200db5789b8f3afe6cdddab4e4bf4f5b
Emily Nakamura <enakamura@ucsd.edu>
2024-07-11 12:50:18 -0700

    modified:   src/DebuggingThermomechnical.py new file:   src/Temperatures.py modified:   src/ThermomechanicalModel.py

--- src/ThermomechanicalModel.py
+++ src/ThermomechanicalModel.py
@@ -193,7 +193,10 @@ class ParametricFireCurve:
         return 3600 / self.time_step_seconds
 
     def rounded_fictitous_duration(self):  # round the fictitious duration
-        return np.ceil(self.fictitious_time() * self.time_steps_coefficient()) / self.time_steps_coefficient()
+        return (
+            np.ceil(self.fictitious_time() * self.time_steps_coefficient())
+            / self.time_steps_coefficient()
+        )
 
     def max_fire_temp(self):
         return 20 + 1325 * (
@@ -222,9 +225,11 @@ class ParametricFireCurve:
         # round the total fictitious duration
 
     def rounded_total_fictitous_duration(self):  # round the fictitious duration
-        return np.ceil(self.total_fictitious_duration() * self.time_steps_coefficient()) / self.time_steps_coefficient()
-          # ceil; see standard library numpy
-        
+        return (
+            np.ceil(self.total_fictitious_duration() * self.time_steps_coefficient())
+            / self.time_steps_coefficient()
+        )
+        # ceil; see standard library numpy
 
         # derive the time array from t = 0 to t = total fictitious duration
 
@@ -232,7 +237,9 @@ class ParametricFireCurve:
         t = np.linspace(
             0,
             self.rounded_total_fictitous_duration(),
-            int(self.time_steps_coefficient() * self.rounded_total_fictitous_duration()),
+            int(
+                self.time_steps_coefficient() * self.rounded_total_fictitous_duration()
+            ),
         )
         return t
 
@@ -252,7 +259,9 @@ class ParametricFireCurve:
                         t - self.rounded_fictitous_duration() * self.x()
                     )  # applies for max rounded fictitious duration less than or equal to 0.5
                 elif 0.5 < self.rounded_fictitous_duration() <= 2.0:
-                    T = self.max_fire_temp() - 250 * (3 - self.rounded_fictitous_duration()) * (
+                    T = self.max_fire_temp() - 250 * (
+                        3 - self.rounded_fictitous_duration()
+                    ) * (
                         t - self.rounded_fictitous_duration() * self.x()
                     )  # applies for max rounded fictitious duration greater than 0.5 and less than or equal to 2.0
                 else:
@@ -270,12 +279,104 @@ class ParametricFireCurve:
 # this need be rounded up to the nearest 5s or 30s, then use
 
 
-# MATERIAL MODULE
-class MaterialProperty:
-    def __init__(self, T_s):
-        self.steel_temperature = T_s
-        return self.steel_temperature
+# EUROCODE REDUCTION FACTOR MODULE
+class SteelReductionFactors:
+    def __init__(self, temperature_steel):
+        self.steel_element_temperature = temperature_steel
+        self.eurocode_temperatures = np.array(
+            [20, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200]
+        )
+        self.reduction_factor_for_effective_yield_strength = np.array(
+            [
+                1.000,
+                1.000,
+                1.000,
+                1.000,
+                1.000,
+                0.780,
+                0.470,
+                0.230,
+                0.110,
+                0.060,
+                0.040,
+                0.020,
+                0.000,
+            ]
+        )
+        self.reduction_factor_for_proportional_limit = np.array(
+            [
+                1.000,
+                1.000,
+                0.807,
+                0.613,
+                0.420,
+                0.360,
+                0.180,
+                0.075,
+                0.050,
+                0.0375,
+                0.0250,
+                0.0125,
+                0.0000,
+            ]
+        )
+        self.reduction_factor_for_youngs_modulus = np.array(
+            [
+                1.000,
+                1.000,
+                0.900,
+                0.800,
+                0.700,
+                0.600,
+                0.310,
+                0.130,
+                0.090,
+                0.0675,
+                0.0450,
+                0.0225,
+                0.0000,
+            ]
+        )
 
+    # Eurocode reduction factors
+    def interpolate_reduction_factor_effective_yield_strength(self):
+        return np.interp(
+            self.steel_element_temperature,
+            self.eurocode_temperatures,
+            self.reduction_factor_for_effective_yield_strength,
+        )
+
+    def interpolate_reduction_factor_proportional_limit(self):
+        return np.interp(
+            self.steel_element_temperature,
+            self.eurocode_temperatures,
+            self.reduction_factor_for_proportional_limit,
+        )
+
+    def interpolate_reduction_factor_youngs_modulus(self):
+        return np.interp(
+            self.steel_element_temperature,
+            self.eurocode_temperatures,
+            self.reduction_factor_for_youngs_modulus,
+        )
+
+
+# MATERIAL MODULE
+class SteelMaterialProperty:
+    def __init__(
+        self,
+        temperature_steel,
+        room_temperature_yield_strength,
+        proportional_limit,
+        room_temperature_youngs_modulus,
+    ):
+        self.steel_temperature = temperature_steel
+        self.room_temperature_youngs_modulus = room_temperature_youngs_modulus
+        self.room_temperature_yield_strength = room_temperature_yield_strength
+        self.proportional_limit = proportional_limit
+        return
+
+    # VERIFY specific heat and thermal conductivity to the Eurocode
     def specific_heat(self):
         # Define the specific heat constant for the time step
         if 20 <= self.steel_temperature < 600:
@@ -310,113 +411,25 @@ class MaterialProperty:
     def density(self):
         return
 
-    def youngs_modulus(self):
-        return
-
-    def yield_strength(self):
-        return
-
-
-# PROTECTIVE MATERIAL MODULE
-class ProtectionProperty:
-    def __init__(
-        self,
-        thickness_of_insulation,
-        specific_heat_insulation,
-        density_of_insulation,
-        thermal_conductivity_of_insulation,
-    ):
-        self.thickness = thickness_of_insulation
-        self.specific_heat = specific_heat_insulation
-        self.density = density_of_insulation
-        self.thermal_conductivity = thermal_conductivity_of_insulation
-
-
-# STEEL TEMP MODULE
-class ProtectedSteelTemperature:
-    def __init__(
-        self,
-        T_f0,
-        T_f,
-        T_s,
-        t0,
-        t,
-        protection,
-        section_type,
-        F,
-        V,
-        # specific_heat_insulation,  # potentially create separate classes (unprotected // protected)
-        # density_insulation,
-        # thermal_conductivity_insulation,
-        # thickness_insulation,
-        convective_heat_transfer_coefficient,
-        stefan_boltzmann_coefficient=56.7 * 10 ^ (-12),
-    ):
-        self.fire_initial_temperature = T_f0
-        self.fire_final_temperature = T_f
-        self.steel_temperature = T_s
-
-        self.time_initial = t0
-        self.time_final = t
-
-        self.protection_type = protection
-        self.section_type = section_type
-
-        self.convective_heat_transfer_coeff = convective_heat_transfer_coefficient
-        self.stefan_boltzmann_coefficient = stefan_boltzmann_coefficient
-
-        self.section_surface_area_of_unit_length_member = F
-        self.section_volume_per_unit_length_member = V
-
-        # self.insulation_specific_heat = specific_heat_insulation
-        # self.insulation_density = density_insulation
-        # self.insulation_thermal_conductivity = thermal_conductivity_insulation
-        # self.insulation_thickness = thickness_insulation
-
-    def delta_t(self):
-        delta_t = self.time_final - self.time_initial
-        if delta_t * ParametricFireCurve.time_steps_coefficient <= 30:
-            d = delta_t
-        elif delta_t * ParametricFireCurve.time_steps_coefficient > 30:
-            d = "Error. Time step is greater than 30 seconds. Time array is not properly spaced for protected case."
-        return d
-
-    def delta_T_f(self):
-        return self.fire_final_temperature - self.fire_initial_temperature
-
-    def section_factor(self):
-        self.contour_protection_section_factor = (
-            self.section_surface_area_of_unit_length_member
-            / self.section_volume_per_unit_length_member
+    def effective_yield_strength(self):
+        return (
+            self.room_temperature_youngs_modulus
+            * SteelReductionFactors.interpolate_reduction_factor_effective_yield_strength()
         )
-        return self.contour_protection_section_factor
 
-    def protection_constant(self):
-        phi = (ProtectionProperty.density * ProtectionProperty.specific_heat) / (
-            MaterialProperty.density * MaterialProperty.specific_heat
+    def effecive_proportional_limit(self):
+        return (
+            self.proportional_limit
+            * SteelReductionFactors.interpolate_reduction_factor_proportional_limit()
         )
-        return phi
 
-    def temperature_change_protected_steel_member(self):
-        temperature_change_per_time_step = (
-            (ProtectionProperty.thermal_conductivity * self.section_factor)
-            / (
-                ProtectionProperty.thickness
-                * ProtectionProperty.density
-                * ProtectionProperty.specific_heat
-            )
-        ) * (self.fire_temperature - self.steel_temperature) / (
-            1 + self.protection_constant / 3
-        ) * (
-            self.time_final - self.time_initial
-        ) - (
-            np.exp(self.protection_constant / 10) - 1
-        ) * (
-            self.fire_final_temperature - self.fire_initial_temperature
+    def effective_youngs_modulus(self):
+        return (
+            self.room_temperature_youngs_modulus
+            * SteelReductionFactors.interpolate_reduction_factor_youngs_modulus()
         )
-        return temperature_change_per_time_step
 
-
+# UNPROTECTED STEEL ELEMENT
 class UnprotectedSteelTemperature:
     def __init__(
         self,
@@ -425,7 +438,6 @@ class UnprotectedSteelTemperature:
         T_s,
         t0,
         t,
-        protection,
         section_type,
         F,
         V,
@@ -434,7 +446,7 @@ class UnprotectedSteelTemperature:
         convective_heat_transfer_coefficient,
         fire_emissivity=1.0,
         material_emissivity=0.7,
-        stefan_boltzmann_coefficient=56.7 * 10 ^ (-12),
+        stefan_boltzmann_coefficient=56.7 * 10 ** (-12),
     ):
 
         self.fire_initial_temperature = T_f0
@@ -445,7 +457,6 @@ class UnprotectedSteelTemperature:
         self.time_initial = t0
         self.time_final = t
 
-        self.protection_type = protection
         self.section_type = section_type
 
         self.convective_heat_transfer_coeff = convective_heat_transfer_coefficient
@@ -459,14 +470,14 @@ class UnprotectedSteelTemperature:
         self.fire_emissivity = fire_emissivity
         self.material_emissivity = material_emissivity
 
-        self.steel_specific_heat = MaterialProperty.specific_heat
-        self.steel_density = MaterialProperty.density
+        self.steel_specific_heat = SteelMaterialProperty.specific_heat()
+        self.steel_density = SteelMaterialProperty.density()
 
     def delta_t(self):
         delta_t = self.time_final - self.time_initial
-        if delta_t * ParametricFireCurve.time_steps_coefficient <= 5:
+        if delta_t * ParametricFireCurve.time_steps_coefficient() <= 5:
             d = delta_t
-        elif delta_t * ParametricFireCurve.time_steps_coefficient > 5:
+        elif delta_t * ParametricFireCurve.time_steps_coefficient() > 5:
             d = "Error. Time step is greater than 5 seconds. Time array is not properly spaced for unprotected case."
         return d
 
@@ -514,8 +525,109 @@ class UnprotectedSteelTemperature:
             )
         return temperature_change_per_time_step
 
+# PROTECTIVE MATERIAL MODULE
+class ProtectionProperty:
+    def __init__(
+        self,
+        thickness_of_insulation,
+        specific_heat_insulation,
+        density_of_insulation,
+        thermal_conductivity_of_insulation,
+    ):
+        self.thickness = thickness_of_insulation
+        self.specific_heat = specific_heat_insulation
+        self.density = density_of_insulation
+        self.thermal_conductivity = thermal_conductivity_of_insulation
 
-# STEEL TEMPERATURE CALCULATION
+# PROTECTED STEEL ELEMENT
+class ProtectedSteelTemperature:
+    def __init__(
+        self,
+        protection,
+        section_type,
+        F,
+        V,
+        # specific_heat_insulation,  # potentially create separate classes (unprotected // protected)
+        # density_insulation,
+        # thermal_conductivity_insulation,
+        # thickness_insulation,
+        convective_heat_transfer_coefficient,
+        T_f0,
+        T_f,
+        T_s,
+        t0,
+        t,
+        stefan_boltzmann_coefficient=56.7 * 10 ^ (-12),
+    ):
+        self.fire_initial_temperature = T_f0
+        self.fire_final_temperature = T_f
+        self.steel_temperature = T_s
+
+        self.time_initial = t0
+        self.time_final = t
+
+        self.protection_type = protection
+        self.section_type = section_type
+
+        self.convective_heat_transfer_coeff = convective_heat_transfer_coefficient
+        self.stefan_boltzmann_coefficient = stefan_boltzmann_coefficient
+
+        self.section_surface_area_of_unit_length_member = F
+        self.section_volume_per_unit_length_member = V
+
+        # self.insulation_specific_heat = specific_heat_insulation
+        # self.insulation_density = density_insulation
+        # self.insulation_thermal_conductivity = thermal_conductivity_insulation
+        # self.insulation_thickness = thickness_insulation
+
+    def delta_t(self):
+        delta_t = self.time_final - self.time_initial
+        if delta_t * ParametricFireCurve.time_steps_coefficient() <= 30:
+            d = delta_t
+        elif delta_t * ParametricFireCurve.time_steps_coefficient() > 30:
+            d = "Error. Time step is greater than 30 seconds. Time array is not properly spaced for protected case."
+        return d
+
+    def delta_T_f(self):
+        return self.fire_final_temperature - self.fire_initial_temperature
+
+    def section_factor(self):
+        self.contour_protection_section_factor = (
+            self.section_surface_area_of_unit_length_member
+            / self.section_volume_per_unit_length_member
+        )
+        return self.contour_protection_section_factor
+
+    def protection_constant(self):
+        phi = (ProtectionProperty.density * ProtectionProperty.specific_heat) / (
+            SteelMaterialProperty.density() * SteelMaterialProperty.specific_heat()
+        )
+        return phi
+
+    def temperature_change_protected_steel_member(self):
+        temperature_change_per_time_step = (
+            (ProtectionProperty.thermal_conductivity * self.section_factor())
+            / (
+                ProtectionProperty.thickness
+                * ProtectionProperty.density
+                * ProtectionProperty.specific_heat
+            )
+        ) * (self.fire_temperature - self.steel_temperature) / (
+            1 + self.protection_constant() / 3
+        ) * (
+            self.time_final - self.time_initial
+        ) - (
+            np.exp(self.protection_constant() / 10) - 1
+        ) * (
+            self.fire_final_temperature - self.fire_initial_temperature
+        )
+        return temperature_change_per_time_step
+
+
+
+
+
+# TEMPERATURES CALCULATIONS
 
 
 # IMPORTING STUFF FROM DATABASE
@@ -524,55 +636,97 @@ class Element:
         return
 
 
-# DEMAND MODULE
-class Demand:
-    def __init__(self, dead_load, live_load, eccentricity, thermal_expansion_coefficient, cross_sectional_area):
-        self.dead_load = dead_load
-        self.live_load = live_load
-        self.thermal_expansion_coefficient = thermal_expansion_coefficient
-        self.cross_sectional_area = cross_sectional_area
-
-    def internal_force(self):
-        return self.thermal_expansion_coefficient * self.cross_sectional_area * temperature_change_array * youngs_modulus_of_elasticity
-
-    def applied_column_load(self):
-        P = 1.2 * self.dead_load + 1.6 * self.live_load
-        return 
-
-    def applied_beam_load(self):
-        w = 1.2 * self.dead_load + 1.6 * self.live_load
-        return
-
-    def column(self):
-        return
-
-    def beam(self):
-        return
-
-
 # CAPACITY MODULE
 class ColumnCapacity:
-    def __init__(self, youngs_modulus_of_elasticity, moment_of_inertia, element_length, eccentricity):
+    def __init__(
+        self,
+        youngs_modulus_of_elasticity,
+        moment_of_inertia,
+        element_length,
+        eccentricity,
+    ):
         self.I = moment_of_inertia
         self.E = youngs_modulus_of_elasticity
         self.L = element_length
-    
+
     def critical_buckling_load(self):
         P = np.pi**2 * self.E * self.I / (self.L**2)
         return
 
-    def column(self):
-        self
-        return
+
+# DEMAND MODULE
+class ColumnDemand:
+    def __init__(
+        self,
+        dead_load,
+        live_load,
+        eccentricity,
+        DCR,
+        thermal_expansion_coefficient,
+        cross_sectional_area,
+    ):
+        self.dead_load = dead_load
+        self.live_load = live_load
+        self.eccentricity = eccentricity
+        self.thermal_expansion_coefficient = thermal_expansion_coefficient
+        self.cross_sectional_area = cross_sectional_area
+
+    def thermal_load(self):
+        P_thermal = 0
+        # P_thermal = self.thermal_expansion_coefficient * self.cross_sectional_area * temperature_change_array * youngs_modulus_of_elasticity
+        return P_thermal
+
+    def applied_vertical_column_load(self):
+        P_applied = 1.2 * self.dead_load + 1.6 * self.live_load
+        return P_applied
+
+    def column_load(self, DCR):
+        P = DCR * ColumnCapacity.critical_buckling_load()
+        # P = P_thermal + P_applied
+        return P
+
+
 
 class BeamCapacity:
     def __init__(self, youngs_modulus_of_elasticity, moment_of_inertia, element_length):
-        r = P
-
-    def column(self):
-        self
+        return
+    def beam(self):
         return
 
+
+# FOR NOW, the demand will be equal to the DCR * the capacity
+
+# PLOT this capacity for the simplest condition compared to the constant demand
+
+# THEN plot this capacity for different eccentricity values compared to the constant demand
+
+
+class BeamDemand:
+    def __init__(
+        self,
+        distributed_dead_load,
+        distributed_live_load,
+        eccentricity,
+        thermal_expansion_coefficient,
+        cross_sectional_area,
+    ):
+        self.distributed_dead_load = distributed_dead_load
+        self.distributed_live_load = distributed_live_load
+        self.eccentricity = eccentricity
+        self.thermal_expansion_coefficient = thermal_expansion_coefficient
+        self.cross_sectional_area = cross_sectional_area
+
+    def thermal_load(self):
+        P_thermal = 0
+        # P_thermal = self.thermal_expansion_coefficient * self.cross_sectional_area * temperature_change_array * youngs_modulus_of_elasticity
+        return P_thermal
+
+    def applied_distributed_beam_load(self):
+        w_applied = 1.2 * self.distributed_dead_load + 1.6 * self.distributed_live_load
+        return w_applied
+
+    def beam(self):
+        return
 
 
 # FAILURE EVALUATION
