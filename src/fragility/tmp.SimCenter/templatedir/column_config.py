30e634532f5ea5a32324131885bfc2862bd50fa2
Emily Nakamura <enakamura@ucsd.edu>
2024-08-13 13:40:17 -0700

    new file:   src/fragility/ColumnDatabase.csv 	new file:   src/fragility/FragilityCurve.pdf 	new file:   src/fragility/FragilityCurveIllustration.pdf 	new file:   src/fragility/W30X124_0_25_90.txt 	new file:   src/fragility/W30X124_0_25_95.txt 	new file:   src/fragility/W30X124_0_29_90.txt 	new file:   src/fragility/W30X124_0_29_95.txt 	new file:   src/fragility/finish.txt 	modified:   src/fragility/fragility_setup.ipynb 	new file:   src/fragility/start.txt 	modified:   src/fragility/tmp.SimCenter/dakota.in 	modified:   src/fragility/tmp.SimCenter/templatedir/column_config.py 	renamed:    src/fragility/tmp.SimCenter/templatedir/dakota.in -> src/fragility/tmp.SimCenter/templatedir/dakota__.in

--- src/fragility/tmp.SimCenter/templatedir/column_config.py
+++ src/fragility/tmp.SimCenter/templatedir/column_config.py
@@ -1,6 +1,6 @@
 # column/component-specific parameters:
-DCR = 0.5
-e = 0 # 0.00300645
-L = 13 / 3.281  # m
-section_size = 'W12X19'
-fire_load_fuel_energy_density = 2000  # MJ/m^2
+DCR = <DCR>
+e = <e> # 0.00300645
+L = <L> / 3.281 # 13 / 3.281  # m
+section_size = <section_size> # 'W30X124' # 'W14X68' # #'W27X102' #'W30X116' #'W14X342' #'W12X19'
+fire_load_fuel_energy_density = <fire_load_fuel_energy_density> # 2000  # MJ/m^2
